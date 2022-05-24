81df4b2fe1c7d1b3cc46aa651c4606d71c5cda03
Газизов Рустам Рафаилович <gazizov@ONIL-ATO.local>
2022-05-24 12:15:39 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+def estimatedValue(price):
+    estimated = price * 0.60
+    return estimated
+
+
+def main():
+    price = 10000
+    total = estimatedValue(price)
+    return total
+
+
+print(main())
